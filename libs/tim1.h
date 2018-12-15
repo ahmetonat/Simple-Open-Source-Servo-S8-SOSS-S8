f4acf3dd67cd46b09e353b98b3847d0b03fce071
Ahmet Onat <onat@sabanciuniv.edu>
2018-12-15 21:23:13 +0300

    Increased encoder range, removed prescaler

--- libs/tim1.h
+++ libs/tim1.h
@@ -2,12 +2,10 @@
 #ifndef __TIM1_H__
 #define __TIM1_H__
 
-#define ENC_RANGE 3200
-#define ENC_ABS_RANGE 1600
-#define ENC_Prescaler 10 //Used in TIM1 prescaler. Perhaps not used for encoder?
-                         //TODO: Check!
+#define ENC_RANGE 10000
+#define ENC_ABS_RANGE 50000
+#define ENC_Prescaler 1 //Used in TIM1 prescaler.
 
 void TIM1_setup(void);
-
 
 #endif
