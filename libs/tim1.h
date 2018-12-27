f5fe01452a3cfa2a42bc04a4df200414966bd21a
Ahmet Onat <onat@sabanciuniv.edu>
2018-12-27 22:39:47 +0300

    Corrected encoder prescaler value.
    TIM1 used as an encoder is intended to have no prescaler. This is not well documented in the manual.
    Previously the prescaler value was set as 1 which resulted in a final prescaler value of 2 (1+1).
    An even final value of the prescaler results in encoder drift.
    This fixes the issue.

--- libs/tim1.h
+++ libs/tim1.h
@@ -4,7 +4,13 @@
 
 #define ENC_RANGE 10000
 #define ENC_ABS_RANGE 50000
-#define ENC_Prescaler 1 //Used in TIM1 prescaler.
+
+//AO!: Prescaler in the encoder mode is not documented well. 
+//If the final value is even, the encoder will not work correctly and drift.
+//The final prescaler value will be the value below +1. 
+//for a good explanation, see:
+//https://stackoverflow.com/questions/39450610/stm32f4-encoder-count-is-changing-when-it-should-not
+#define ENC_Prescaler 0 //Used in TIM1 prescaler.
 
 void TIM1_setup(void);
 
